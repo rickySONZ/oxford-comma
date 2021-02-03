52d2de7923214cc0d65e2ae598699397a1f62a9d
Ryan Erricson <ryanerricson@gmail.com>
2021-02-03 13:13:37 -0500

    commit tree 4869b7a74896722f21b6ef30da2ecf42faa92927

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array_to_string = array.join
+        return array_to_string
+    elsif array.length == 2
+        two_element_string = array.join(" and ")
+        return two_element_string
+    else
+        array.insert(array.length-1, "and #{array[array.length - 1]}")
+        array.pop()
+        multiple_element_string = array.join(", ")
+        return multiple_element_string
+    end
 end
